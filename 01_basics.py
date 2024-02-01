ae51bd1dbc77d81663516ce795ff456b6bc4ca9b
Sivakumar Revuri <sivakumarr96@gmail.com>
2024-02-01 12:42:48 +0530

    day1: basics

---
+++ 01_basics.py
@@ -0,0 +1,72 @@
+
+
+# print("Hello World!!!") # output statement
+
+# name = input("please enter your name")
+
+# # this is print function
+# print(name, type(name))
+# name = int(name)
+# print(name, type(name))
+
+
+# test = "test"
+
+# print(test)
+
+# import keyword
+
+# print(keyword.kwlist)
+
+
+
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+
+
+# age:int = 23
+
+# print(age)
+
+# salary: float = 2343.45
+# print(salary, type(salary))
+
+
+# # String
+
+# string = 'Akshara'
+
+
+# print(string, type(string))
+# print(string.upper())
+
+
+# Conditional statements
+
+# name = "Subhash"
+# age = 26 if name == "Subhsh" else 40, 'test', 'test2'
+
+# print(age, type(age))
+
+
+# Datatypes:
+#         numbers: int, float
+#         boolean: True, False
+#         strings: '', "", """triple quote"""
+#         complex: a+bj; a-real and b-img
+
+
+name: str = input('please enter your name: ')
+age: str = input(f'{name} enter your age: ')
+
+age: int = int(age)
+
+if age >= 18:
+    print('you are eligible for vote')
+else:
+    print('not eligible please come once you are 18')
+
+
+
+
+
