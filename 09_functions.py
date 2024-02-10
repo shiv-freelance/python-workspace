9667c78fe2395f8efa72b1fdbdbde6eb5c09b75b
shiv-freelance <python.fullstack.freelancer@gmail.com>
2024-02-10 11:17:35 +0530

    week-1-programs

--- 09_functions.py
+++ 09_functions.py
@@ -35,10 +35,10 @@ b = 30.5
 def sub(a, b):
     return a-b
 
-a = 10
-b = 20
-print(sub(a, b)) # -10, b = 20, a = 10
-print(sub(b=b, a=a)) # 10, b = 20, a = 10
+# a = 10
+# b = 20
+# print(sub(a, b)) # -10, b = 20, a = 10
+# print(sub(b=b, a=a)) # 10, b = 20, a = 10
 
 # Rules
 # 1. it should be in same sequence.
@@ -51,28 +51,83 @@ print(sub(b=b, a=a)) # 10, b = 20, a = 10
 
 
 #1. positional
-def sub(a, b):
-    return a-b
+# def sub(a, b):
+#     return a-b
 
-a = 10
-b = 20
-print(sub(a, b)) # -10, b = 20, a = 10
-print(sub(b, a)) # 10, b = 20, a = 10
-
-
-# 2. keyword args:
-def sub(a, b):
-    return a-b
-
-a = 10
-b = 20
-print(sub(a, b)) # -10, b = 20, a = 10
-print(sub(b=b, a=a)) # -10, b = 20, a = 10
+# a = 10
+# b = 20
+# print(sub(a, b)) # -10, b = 20, a = 10
+# print(sub(b, a)) # 10, b = 20, a = 10
 
 
-# 3. default
-def hello(name="Bharat"):
-    return f"Hello, {name}"
+# # 2. keyword args:
+# def sub(a, b):
+#     return a-b
 
-print(hello("nagesh"))
-print(hello())
+# a = 10
+# b = 20
+# print(sub(a, b)) # -10, b = 20, a = 10
+# print(sub(b=b, a=a)) # -10, b = 20, a = 10
+
+
+# # 3. default
+# def hello(name="Bharat"):
+#     return f"Hello, {name}"
+
+# print(hello("nagesh"))
+# print(hello())
+
+
+#4. variable length arguments.
+nums = 1
+nums = 1
+# print(nums, type(nums))
+
+# def test(a, b, c=10):
+#     print(a, b, c)
+
+# test(10, 20, 30) # 10, 20, 30
+# test(10, 30) # 10, 30
+# # test(10)
+
+# *args-tuple
+
+
+# def test1(*args):
+#     print(args, type(args))
+
+
+# test1(10, 20, 30, 40, 50, 60)
+# test1(10)
+# test1()
+
+
+
+# # **kwargs - dict
+# def sub(a, b):
+#     return a-b
+
+# a = 10
+# b = 20
+# print(sub(a, b)) # -10, b = 20, a = 10
+# print(sub(b=b, a=a)) # -10, b = 20, a = 10
+
+def test(**names):
+    print(names, type(names)) # {'a':10, 'b': 20}, dict
+    for key in names.keys(): # keys
+        print(key)
+    # this for loop prints all values
+    for value in names.values():
+        print(value)
+
+    for key, value in names.items(): # key, value
+        print(key, value)
+
+    for item in names.items(): #tuple
+        print(item, type(item))
+
+
+# test(a = 10)    
+# test(a=19, c=100)
+
+test()
--- week-1-programs.py
+++ week-1-programs.py
@@ -1,38 +1,69 @@
 
-a: int = int(input('first value: '))
-b: int = int(input('second value: '))
+# a: int = int(input('first value: '))
+# b: int = int(input('second value: '))
 
-print(a+b)
+# print(a+b)
 
-for i in range(50):
-    print(i, end=' ')
+# for i in range(50):
+#     print(i, end=' ')
 
-# write a program to print from 1000 to 1010
-for i in range(1000, 1010, 1):
-    print(i, end='\t')
+# # write a program to print from 1000 to 1010
+# for i in range(1000, 1010, 1):
+#     print(i, end='\t')
 
-# even numbers
-nums = [2,6,34,32,11, 9, 15, 22]
+# # even numbers
+# nums = [2,6,34,32,11, 9, 15, 22]
 
-for i in nums:
-    if i % 2 != 0:
-        print(i)
+# for i in nums:
+#     if i % 2 != 0:
+#         print(i)
 
-for i in range(len(nums)):
-    print(i, nums[i])
+# for i in range(len(nums)):
+#     print(i, nums[i])
 
 
-fruits = ['apple', 'banana', 'mango', 'orange']
+# fruits = ['apple', 'banana', 'mango', 'orange']
 
-for fruit in fruits:
-    if 'e' not in fruit:
-        print(fruit)
-    elif 'a' not in fruit:
-        print(fruit)
+# for fruit in fruits:
+#     if 'e' not in fruit:
+#         print(fruit)
+#     elif 'a' not in fruit:
+#         print(fruit)
 
-value = -10
+# value = -10
 
-if value < 15 and value > 0:
-    print('inside if')
-else:
-    print('else block')
+# if value < 15 and value > 0:
+#     print('inside if')
+# else:
+#     print('else block')
+
+
+# 12345 o/p: 54321
+
+# string
+num = 12345
+# num_str = str(12345)
+
+# result = int(num_str[::-1])
+
+# # print(result, type(result))
+
+
+# result = 0
+
+# while num > 0:
+#     result = result * 10 + num % 10
+#     num = num //10
+
+# print(result)
+
+
+
+result = ""
+
+while num > 0:
+    result = result + str(num % 10)
+    # result += num % 10
+    num = num //10
+
+print(int(result))
