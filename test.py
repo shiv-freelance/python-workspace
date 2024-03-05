0c60b7ddb766fd52bbda05a8d5245e9df57f0b4d
SivakumarRevuri <154948859+shiv-freelance@users.noreply.github.com>
2024-03-05 08:28:19 +0530

    moduels

---
+++ test.py
@@ -0,0 +1,14 @@
+# import oops.person
+
+# p = oops.person.Person()
+
+
+# # DRY - Don't repeat yourself
+
+# name = oops.person.name
+
+
+from oops.person import name, age, mail, Person
+
+# p = Person()
+print(name, age, mail)
