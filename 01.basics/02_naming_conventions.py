7bfcec7e677cc6e910043fb0b17002bfa501eaa7
SivakumarRevuri <154948859+shiv-freelance@users.noreply.github.com>
2024-03-11 11:14:07 +0530

    adding naming conventions

---
+++ 01.basics/02_naming_conventions.py
@@ -0,0 +1,11 @@
+
+# Naming variable
+
+# 1. it should allow only alpha numeric (a-zA-Z0-9), 1 special char (_)
+# 2. 1st character shouldn't be a number, but underscore will be allowed.
+# 3. any other special char instead of _ will give error.
+# 4. keywords shouldn't be used to declare a variable
+# await = "I am good"
+
+
+# when ever we are declaring a variable a we follow snake_case
