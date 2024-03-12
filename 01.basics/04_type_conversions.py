3fc84137e6267b406d09c5d76eee7490c9575052
SivakumarRevuri <154948859+shiv-freelance@users.noreply.github.com>
2024-03-12 09:13:16 +0530

    type conversions

--- 01.basics/04_type_conversions.py
+++ 01.basics/04_type_conversions.py
@@ -1,22 +1,58 @@
-num = int("25")  # type conversion
+
+# 5 fundamental data types:
+
+# 1. int
+# 2. float
+# 3. string
+# 4. boolean
+# 5. complex
+
+# type conversions: Convert from one datatype to another datatype
+# ex: int -> float; boolean -> int
+
+# type conversion functions:
+
+# 1. int(value): value the one which needs to converted to int
+# 2. float(value)
+# 3. string(value)
+# 4. bool(value)
+# 5. complex(value)
+
+decimal:float = 34.98
+flag: bool = True
+string_val:str = 'Siddhu'
+int_val:int = 100
+complex_val: complex = 10+30j #a+ib
+
+# int conversion
+int(decimal) # 34
+int(flag) # 1
+int(False) # 0
+int(string_val) # ValueError (if it's digits it get's converted)
+num = int("25")  # get's converted.
 print(num, type(num))
+# float to int
+result = 22 / 7 # 3.14
+print(result, int(result)) # 3
 
-result = str(454545.34343)  # converting to string using str() function.
-print(type(result))
-print(complex(1))
 
+# float conversion
+float(100) # 100.0
+float(True) # 1.0
+float(False) # 0.0
+
+
+# String conversion; any value passed as an arguemnt get's converted to string datatype
+result = str(454545.34343)  # converting to string using str() function
+print(type(result)) # checking the datatype using type() function
 # string to int
 output = "0930xa9034"
 print(int(output))  # fails it is alpha numeric.
 
-# integer to float conversion.
-result = 45
-print(float(result))  # converting float using float() function
 
-
-# float to int
-result = 22 / 7
-print(result, int(result))
+# complex datatype
+print(complex(1))
+complex(8, 10) # 8+10j
 
 
 # boolean conversion
@@ -24,6 +60,7 @@ flag1 = True
 type_conversion = bool(" ")
 print(type_conversion)
 print(flag1, int(True), int(False))
+
 
 
 # string to int
