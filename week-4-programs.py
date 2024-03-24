fdc0d192dd68a403e37667088002cdc854849236
SivakumarRevuri <154948859+shiv-freelance@users.noreply.github.com>
2024-03-24 23:09:51 +0530

    24th March 2024

--- week-4-programs.py
+++ week-4-programs.py
@@ -5,6 +5,30 @@ output = [(0, 10), (1, 9), (7, 3)]
 
 for i in range(len(nums)):
     for j in range(i + 1, len(nums)):
-        # if nums[i]+nums[j] == 10:
-        #     print(nums[i],nums[j])
-        print(i, j)
+        if nums[i]+nums[j] == 10:
+            print(nums[i],nums[j])
+        # print(i, j)
+
+
+
+# pivot integer
+def pivotInteger(n: int) -> int:
+    nums = list(range(1,n+1))
+    size = len(nums)
+    if size == 1:
+        return n
+    for i in range(1, size):
+        left_vals = nums[:i]
+        right_vals = nums[-1:-size+i:-1]
+        if sum(left_vals) == sum(right_vals):
+            return i
+    return -1
+
+pivotInteger(8)
+############### (or) ##############
+from math import sqrt
+
+def pivotInteger(n: int):
+    result = sqrt(n*(n+1)/2)
+    return -1 if result % 1 != 0 else int(result)
+
