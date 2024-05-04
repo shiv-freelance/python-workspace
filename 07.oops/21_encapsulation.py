da5dbcb05cbe9b7cdd48646c99f65131683a8399
SivakumarRevuri <154948859+shiv-freelance@users.noreply.github.com>
2024-05-04 20:35:39 +0530

    04th May 2024

---
+++ 07.oops/21_encapsulation.py
@@ -0,0 +1,22 @@
+class Person:
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        self.__name = name
+
+    def get_age(self):
+        return self.__age
+
+    def set_age(self, age):
+        self.__age = age
+        
+p = Person("shiv", 27)
+
+print(Person.__dict__)
+
+print(p.get_age(), p.get_name())
